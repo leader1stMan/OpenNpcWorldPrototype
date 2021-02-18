90a6d5bdfb6ed4af30eb76c7496baa9ed828fabc
Pavel418 <pavel@vvv.az>
2021-02-19 01:02:31 +0400

    quest script changed

---
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public List<Quest> quests;
+
+    private void Awake()
+    {
+        quests = new List<Quest>()
+        {
+            TreasonQuest.instance
+        };
+    }
+}
